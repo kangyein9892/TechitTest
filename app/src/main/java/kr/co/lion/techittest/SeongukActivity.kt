cf59283098a0be29365b47709c1660408be67a12
seongukHeo <heoseonguk3@gmail.com>
2024-03-15 01:44:01 +0900

    #10 Feat : Activity 추가입니다

---
+++ app/src/main/java/kr/co/lion/techittest/SeongukActivity.kt
@@ -0,0 +1,13 @@
+package kr.co.lion.techittest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeongukActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seonguk)
+
+        //추가합니다
+    }
+}
